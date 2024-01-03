6f4970d03b4e61a1769b7bcdd318950a1fc57d05
Bhawish Kumar <bhawishkumar000@gmail.com>
2024-01-04 00:07:57 +0800

    first

---
+++ main.js
@@ -0,0 +1,16 @@
+// game elements
+const canvas=document.getElementById("cvs");
+const ctx=canvas.getContext("2d");
+
+// game functions
+let FPS=1000/15;
+let gameLoop;
+let snake_height=15;
+let snake_x=200;
+let snake_y=200;
+const snake_width=25;
+let FoodEaten=false;
+
+function frame(){
+
+}
